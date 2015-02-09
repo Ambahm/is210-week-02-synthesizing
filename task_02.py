72f5f11dc187079c09422e47d1e5a55b17adec13
Ambreen Ahmed <ambahm.aa@gmail.com>
2015-02-09 05:22:33 +0000

    Wk 2 Synthesizing task2: Added a docstring

--- task_02.py
+++ task_02.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+""" Wk2: Synthesizing task- Adding a docstring: By convention enclosed in three
+quotation marks, fist line in class/module/function added as brief intro"""
 print "Hello Student!"
