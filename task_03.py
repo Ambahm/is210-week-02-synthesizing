242c2bedfff4d67ab62d3a2f0c30542181111da9
Ambreen Ahmed <ambahm.aa@gmail.com>
2015-02-09 06:48:26 +0000

    Added interpreter and coding codes Added doc string Deleted extra spaes and lines from file

--- task_03.py
+++ task_03.py
@@ -1,7 +1,8 @@
-CHARLIE = "Chocolate Factory" 
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Wk 2 T3: Interpreting and correcting Test results"""
+CHARLIE = "Chocolate Factory"
 VIOLET = "Gray"
 PATRICIA = "Reichardt"
 LINUS = "van Pelt"
-
-
-
+print CHARLIE, VIOLET, PATRICIA, LINUS
